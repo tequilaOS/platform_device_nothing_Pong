6d7d5ce7929ff0dfaca859b321ae4ec8e48a8251
SGCMarkus <markusornik@gmail.com>
2023-10-19 11:53:28 +0200

    Pong: Add security.rkp-V3 dependency to keymint service
    The symbol _ZN4aidl7android8hardware8security7keymint29IRemotelyProvisionedComponent10descriptorE
    was moved from android.hardware.security.keymint-V2-ndk to android.hardware.security.rkp-V3-ndk.
    
    Change-Id: I7f3c40e3e8a2a848cdcb73e5232952f63681aa58
    Signed-off-by: Bartłomiej Rudecki <ejbtrd@gmail.com>

--- device.mk
+++ device.mk
@@ -285,6 +285,7 @@ PRODUCT_PACKAGES += \
     android.hardware.security.keymint-V1-ndk_platform.vendor \
     android.hardware.security.keymint-V1-ndk.vendor \
     android.hardware.security.keymint-V2-ndk.vendor \
+    android.hardware.security.rkp-V3-ndk.vendor \
     android.hardware.security.secureclock-V1-ndk_platform.vendor \
     android.hardware.security.secureclock-V1-ndk.vendor \
     android.hardware.security.sharedsecret-V1-ndk_platform.vendor \
